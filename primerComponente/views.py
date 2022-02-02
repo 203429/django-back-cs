90aeb199294f8c077232df318a981e3b2e6b5672
203429 <203429@ids.upchiapas.edu.mx>
2022-02-02 12:36:01 -0600

    added custom responses

--- primerComponente/views.py
+++ primerComponente/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+import json
 
 # IMPORTACIONES DE MODELOS AGREGADOS
 from primerComponente.models import primerTabla
@@ -11,18 +12,27 @@ from primerComponente.serializers import primerTablaSerializer
 
 # Create your views here.
 class primerTablaList(APIView):
+    def response_custom(self, message, data, status):
+        responseJson = {"message":message, "pay_load":data, "status":status}
+        responseJsonB = json.dumps(responseJson)
+        finalResponse = json.loads(responseJsonB)
+        return finalResponse
+
     def get(self, request, format = None):
         queryset = primerTabla.objects.all()
         serializer=primerTablaSerializer(queryset, many = True, context = {'request':request})
-        return Response(serializer.data)
+        return Response(self.response_custom("Success", serializer.data, status.HTTP_200_OK))
+        # return Response(serializer.data)
 
     def post(self, request, format = None):
         serializer = primerTablaSerializer(data = request.data)
         if serializer.is_valid():
             serializer.save()
             datas = serializer.data
-            return Response(datas, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+            return Response(self.response_custom("Success", datas, status.HTTP_201_CREATED))
+            # return Response(datas, status = status.HTTP_201_CREATED)
+        return Response(self.response_custom("Error", serializer.errors, status.HTTP_400_BAD_REQUEST))
+        # return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
 class primerTablaDetail(APIView):
     def get_object(self, pk):
